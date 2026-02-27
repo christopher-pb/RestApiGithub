a8a2a1c9e0511f24ff7b0d9c57383503b0590a90
AbhiS14 <abhi998677@gmail.com>
2026-02-27 09:49:40 +0530

    Fix health endpoint for Docker

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from app.api import register_blueprints
 
-def create_app():
+
+def create_app(config_name="development"):
     app = Flask(__name__)
 
     register_blueprints(app)
--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,11 +1,13 @@
+from app.api.health import health_bp
 from app.api.employee import employee_bp
 from app.api.department import department_bp
 from app.api.salary import salary_bp
-from app.api.health import health_bp
+from app.api.auth import auth_bp
 
 
 def register_blueprints(app):
+    app.register_blueprint(health_bp)
+    app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(employee_bp, url_prefix="/employees")
     app.register_blueprint(department_bp, url_prefix="/departments")
     app.register_blueprint(salary_bp, url_prefix="/salaries")
-    app.register_blueprint(health_bp)
