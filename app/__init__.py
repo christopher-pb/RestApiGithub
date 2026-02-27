f5ec9f6cf9b4aeef0d746f4dba5913f35e63171f
AbhiS14 <abhi998677@gmail.com>
2026-02-27 07:23:31 +0530

    Fixed create_app function to accept config parameter

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,16 @@ from flask import Flask
 from app.api import register_blueprints
 
 
-def create_app():
+def create_app(config_name="development"):
+    """
+    Application factory
+    Accepts config_name for testing, development, production
+    """
 
     app = Flask(__name__)
+
+    # Optional config (needed for GitHub Actions tests)
+    app.config["TESTING"] = config_name == "testing"
 
     register_blueprints(app)
 
