6fd244c120aa8f16244663af0c008fd0ce05221c
Akshaya R <achur0211@gmail.com>
2026-03-08 20:14:07 +0530

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,7 +3,7 @@ from app.api.employee import employee_bp
 from app.api.department import department_bp
 from app.api.salary import salary_bp
 from app.api.auth import auth_bp
-
+from app.api.students import bp as students_bp
 
 def register_blueprints(app):
     app.register_blueprint(health_bp)
@@ -11,3 +11,5 @@ def register_blueprints(app):
     app.register_blueprint(employee_bp, url_prefix="/employees")
     app.register_blueprint(department_bp, url_prefix="/departments")
     app.register_blueprint(salary_bp, url_prefix="/salaries")
+    api.register_blueprint(students_bp)
+
